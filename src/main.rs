7168b571500bb395ce0c8da3ade62d69022eae30
Roddy MacCrimmon <r.maccrimm@gmail.com>
2019-04-14 19:54:31 -0600

    intersecting sphere works

--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,127 @@
 extern crate sdl2;
-extern crate gl;
-use std::ops::{Add, Sub};
-use std::ffi::{CString, CStr};
+mod vec;
+use sdl2::{
+    rect::Point, 
+    video::Window, 
+    pixels::Color, 
+    render::Canvas
+};
+use std::f64;
 
-fn find_sdl_gl_driver() -> Option<u32> {
-    for (index, item) in sdl2::render::drivers().enumerate() {
-        if item.name == "opengl" {
-            return Some(index as u32);
-        }
-    }
-    None
+#[derive(Debug)]
+struct Ray {
+    dir: vec::R3,
+    pt: vec::R3,
 }
 
-#[derive(Copy, Clone)]
-struct R3 {
-    x: f64,
-    y: f64,
-    z: f64  
+struct Sphere {
+    c: vec::R3,
+    r: f64,
 }
 
-impl Add for R3 {
-    type Output = R3;
-
-    fn add(self, rhs: R3) -> R3 {
-        R3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+fn intersect_sphere(r: Ray, s: &Sphere) -> Option<Ray> {
+    let l = r.dir.normalize();
+    let o = r.pt;
+    let c = s.c;
+    let rad = s.r;
+    let x = (l.dot(o - c)).powi(2) - ((o - c).norm().powi(2) - rad.powi(2));
+    if x < 0.0 {
+        return None;
     }
+    // Take closest (or only if x = 0) intersection point, with dist along r given by
+    let dist = -2.0 * l.dot(o - c) - x.sqrt() / (2.0 * l.norm().powi(2));
+    let inter_pt = o + (dist * l);
+    // Normal to sphere at intersection point
+    let n = (inter_pt - c).normalize();
+    let reflect_dir = 2.0 * (n.dot(l) * n) - l;
+
+    let reflected = Ray { dir: reflect_dir, pt: inter_pt };
+    Some(reflected)
 }
 
-impl Sub for R3 {
-    type Output = R3;
-
-    fn sub(self, rhs: R3) -> R3 {
-        R3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
-    }
-}
-
-impl R3 {
-    fn new(x: f64, y: f64, z: f64) -> R3 {
-        R3 {x, y, z}
-    }
-
-    fn zero() -> R3 {
-        R3 {x:0.0, y:0.0, z:0.0}
-    }
-
-    fn len(&self) -> f64 {
-        let x = self.x.powi(2) + self.y.powi(2) + self.z.powi(2);
-        x.sqrt()
-    }
-
-    fn dot(&self, u: R3) -> f64 {
-        self.x * u.x + self.y * u.y + self.z * u.z
-    }
-
-    fn normalize(&self) -> R3 {
-        let l = self.len();
-        R3 {
-            x: self.x / l, 
-            y: self.y / l,
-            z: self.z / l
-        }
-    }
-}
-
-fn compile_shader(shader_type: gl::types::GLenum, shader_src: &CStr) 
-    -> Result<gl::types::GLenum, String> 
-{
-    // let shader_src = CString::new(source).unwrap();
-    let mut success: gl::types::GLint = 0;
-    let mut err_len: gl::types::GLint = 0;
-    let shader_id: gl::types::GLuint = unsafe { gl::CreateShader(shader_type) };
-    unsafe { 
-        gl::ShaderSource(shader_id, 1, &shader_src.as_ptr(), std::ptr::null());
-        gl::CompileShader(shader_id);
-        gl::GetShaderiv(shader_id, gl::COMPILE_STATUS, &mut success);
-    }
-    if success == 0 {
-        let mut buffer: Vec<u8> = Vec::with_capacity(err_len as usize + 1);
-        // buffer.extend([b' '].iter().cycle().take(err_len as usize));
-        let error: CString = unsafe { CString::from_vec_unchecked(buffer) };
-        unsafe {
-            gl::GetShaderiv(shader_id, gl::INFO_LOG_LENGTH, &mut err_len);
-            gl::GetShaderInfoLog(
-                shader_id,
-                err_len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar
-            );
-        }
-        Err(error.to_string_lossy().into_owned())
-    }
-    else {
-        Ok(shader_id)
-    }
+fn get_canvas(context: &sdl2::Sdl) -> Canvas<Window> {
     
+    let video_subsystem = context.video().unwrap();
+    let window = video_subsystem.window("Window", 800, 600)
+        .build()
+        .unwrap();
+    let canvas = window.into_canvas()
+        .build()
+        .unwrap();
+    canvas
+}
+
+#[derive(Debug)]
+struct Viewport {
+    // Position of camera in space
+    eye: vec::R3,
+    dir: vec::R3,
+    // Horizontal field of view, in radians
+    fov: f64,
+    // Height and width in pixesl
+    h: i32,
+    w: i32,
+    // Distance from eye to viewport
+    clip: f64,
+    // Actual width and height
+    height: f64,
+    width: f64,
+    // Top left corner, relative eye, assumes static dir in z direction
+    origin: vec::R3,
+    dx: f64,
+    dy: f64,
+}
+
+impl Viewport {
+    fn new(w: i32, h: i32, clip: f64) -> Viewport {
+        let eye = vec::R3::new(0.0, 0.0, 0.0);
+        let dir = vec::R3::new(0.0, 0.0, 1.0);
+        let fov: f64 = 90.0;
+        let width = 2.0 * fov.to_radians().sin() * clip;
+        let height = (h as f64) / (w as f64) * width;
+        let x = vec::R3::new(1.0, 0.0, 0.0);
+        let y = vec::R3::new(0.0, 1.0, 0.0);
+        let origin = (eye + clip * dir) - (width / 2.0) * x + (height / 2.0) * y;
+        let dx = width / (w as f64);
+        let dy = height / (h as f64);
+        Viewport { eye, dir, fov, h, w, clip, height, width, origin, dx, dy }
+    }
+
+    fn get_ray(&self, i: i32, j: i32) -> Ray {
+        let o = self.origin + (self.dx * vec::X * (i as f64)) + (self.dy * vec::Y * (-j as f64));
+        let dir = o.normalize() - self.eye;
+        Ray {
+            dir,
+            pt: o,
+        }
+    }
 }
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem.window("Window", 800, 600)
-        .opengl()
-        .build()
-        .unwrap();
-    let gl_context = window.gl_create_context().unwrap();
-    let gl_attr = video_subsystem.gl_attr();
-    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-    gl_attr.set_context_version(3,3);
+    let mut canvas = get_canvas(&sdl_context);
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
 
-    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let viewport = Viewport::new(800, 600, 1.0);
+    let s = Sphere {
+        r: 2.0,
+        c: vec::R3::new(0.0, 0.0, 5.0)
+    };
 
-    // Error messages don't seem to actually work atm
-    match compile_shader(gl::VERTEX_SHADER, &CString::new(include_str!("vertex.glsl")).unwrap()) {
-        Ok(_) => (),
-        Err(msg) => {
-            eprintln!("Error compiling vertex shader: {}", msg);
-            std::process::exit(1);
+    'outer: for i in 0..viewport.w {
+        'inner: for j in 0..viewport.h {
+            let r = viewport.get_ray(i, j);
+            let color = match intersect_sphere(r, &s) {
+                Some(_) => Color::RGB(0, 0, 0),
+                None => Color::RGB(255, 255, 255)
+            };
+            canvas.set_draw_color(color);
+            canvas.draw_point(Point::new(i, j)).unwrap();
         }
     }
+    canvas.present();
 
-    match compile_shader(gl::FRAGMENT_SHADER, &CString::new(include_str!("fragment.glsl")).unwrap()) {
-        Ok(_) => (),
-        Err(msg) => {
-            eprintln!("Error compiling fragment shader: {}", msg);
-            std::process::exit(1);
-        }
-    }
-
-    let mut screen_tex: gl::types::GLuint = 0;
-    unsafe {
-        gl::GenTextures(1, &mut screen_tex);
-        gl::ActiveTexture(gl::TEXTURE0);
-        gl::BindTexture(gl::TEXTURE_2D, screen_tex);
-        gl::TexImage2D(
-            gl::TEXTURE_2D, 
-            0, 
-            gl::RGBA, 
-            160,
-            144,
-            0, 
-            gl::RGBA,
-            gl::FLOAT, 
-            
-        );
-    }
-    
     let mut event_pump = sdl_context.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
